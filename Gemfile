0b4f1f485772872f2828c534670458b662a8b4e9
Budi Sugianto <budisugianto777@gmail.com>
2013-02-11 19:57:55 -0500

    Initialize new apps for Rails training

---
+++ Gemfile
@@ -0,0 +1,30 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.11'
+
+group :production do
+  gem 'pg'
+end
+
+group :development, :test do
+  gem 'sqlite3'
+end
+
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'paperclip'
+  gem 'uglifier', '>= 1.0.3'
+  gem 'paperclip'
+  gem 'bootstrap-sass', '~> 2.2.2.0'
+
+end
+
+gem 'bcrypt-ruby'
+gem 'jquery-rails'
+gem 'therubyracer'
+gem 'devise'
+gem 'simple_form'
+gem 'aws-s3'
+gem "aws-sdk"
+gem 'nokogiri'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChooseCheese::Application.config.secret_token = 'd6cfa9d7544ea9d75b3d9a42227f177e7225d7a84b186a304df3d6a84f329cba3d73df8b5b112898db96d0496327a9a194eead7a382f9483227e33e557f0e389'
