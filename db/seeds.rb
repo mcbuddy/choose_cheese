429a4ec938751d5822e656665883ac35a4f00f60
Budi Sugianto <budisugianto777@gmail.com>
2013-02-12 21:40:40 -0500

    add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-admin = Admin.create(email:'admin@example.com', password:'12345678', password_confirmation:'12345678')
+user = User.create(email:'user@example.com', password:'12345678', password_confirmation:'12345678')
