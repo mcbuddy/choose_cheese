45e7001f8537fa17385fde89ecd34aabe9eb83ad
Budi Sugianto <budisugianto777@gmail.com>
2013-02-12 19:46:16 -0500

    fixing the config prod.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,11 @@ ChooseCheese::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  #TODO need for heroku mailing system
+  config.action_mailer.default_url_options = { :host => '' }
+
+  #required devise gem rn on heroku
+  config.assets.initialize_on_precompile = false
+
 end
