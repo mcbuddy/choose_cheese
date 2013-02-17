df5b31602a9b78254f61c6f04c52f0d3acb7a6e6
Budi Sugianto <budisugianto777@gmail.com>
2013-02-16 21:59:35 -0500

    Adding seed file

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -1,7 +1,7 @@
 class Cheese < ActiveRecord::Base
   attr_accessible :cheese_description, :cheese_name, :milk_type, :origin_country, :pasteurized, :taste, :texture
 
-  validates_presence_of :cheese_name, :cheese_description, :origin_country, :milk_type, :pasteurized, :taste, :texture
+  validates_presence_of :cheese_name, :origin_country, :milk_type, :pasteurized, :taste, :texture
 
   belongs_to :user
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,7 @@
 
 userA = User.create(name:'userA', email:'userA@example.com', password:'12345678', password_confirmation:'12345678')
 userB = User.create(name:'userB', email:'userB@example.com', password:'12345678', password_confirmation:'12345678')
+
+ch1 = Cheese.create(cheese_description: '', cheese_name: 'Manchego', milk_type:'Sheep Milk', origin_country: 'Spain', pasteurized: true, taste:'Mild', texture:'Semi-Hard')
+ch2 = Cheese.create(cheese_description: '', cheese_name: 'Brie', milk_type:'Cow Milk', origin_country: 'French', pasteurized: true, taste:'Mild', texture:'Soft')
+ch3 = Cheese.create(cheese_description: '', cheese_name: 'Parmigiano Reggiano', milk_type:'Cow Milk', origin_country: 'Italy', pasteurized: false, taste:'Sharp', texture:'Hard')
