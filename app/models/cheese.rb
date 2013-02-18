a0073e1581cd65760fa75d9f073583878b4fdc02
Budi Sugianto <budisugianto777@gmail.com>
2013-02-18 09:05:22 -0500

    adding upload file function

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -2,7 +2,7 @@ class Cheese < ActiveRecord::Base
   attr_accessible :image, :cheese_description, :cheese_name, :milk_type, :origin_country, :pasteurized, :taste, :texture
 
   validates_presence_of :cheese_name, :origin_country, :milk_type, :pasteurized, :taste, :texture
-  validates_attachment :image, presence: true, content_type: {content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']}, size: {less_than: 5.megabytes}
+  validates_attachment :image, presence: true, content_type: {content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']}, size: {less_than: 3.megabytes}
 
-  has_attached_file :image
+  has_attached_file :image , :styles => { :medium => "400x300>", :thumb => "100x100>" }
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-userA = User.create(name:'userA', email:'userA@example.com', password:'12345678', password_confirmation:'12345678')
-userB = User.create(name:'userB', email:'userB@example.com', password:'12345678', password_confirmation:'12345678')
+userBudi = User.create(name:'Budi', email:'budi@example.com', password:'12345678', password_confirmation:'12345678')
+userKaye = User.create(name:'Kaye Han', email:'kaye@example.com', password:'12345678', password_confirmation:'12345678')
+userDanny = User.create(name:'Daniel Han', email:'daniel@example.com', password:'12345678', password_confirmation:'12345678')
+userKevin = User.create(name:'Kevin Kim', email:'kevin@example.com', password:'12345678', password_confirmation:'12345678')
 
-ch1 = Cheese.create(cheese_description: '', cheese_name: 'Manchego', milk_type:'Sheep Milk', origin_country: 'Spain', pasteurized: true, taste:'Mild', texture:'Semi-Hard')
-ch2 = Cheese.create(cheese_description: '', cheese_name: 'Brie', milk_type:'Cow Milk', origin_country: 'French', pasteurized: true, taste:'Mild', texture:'Soft')
-ch3 = Cheese.create(cheese_description: '', cheese_name: 'Parmigiano Reggiano', milk_type:'Cow Milk', origin_country: 'Italy', pasteurized: false, taste:'Sharp', texture:'Hard')
+# ch1 = Cheese.create(cheese_description: '', cheese_name: 'Manchego', milk_type:'Sheep Milk', origin_country: 'Spain', pasteurized: true, taste:'Mild', texture:'Semi-Hard')
+# ch2 = Cheese.create(cheese_description: '', cheese_name: 'Brie', milk_type:'Cow Milk', origin_country: 'French', pasteurized: true, taste:'Mild', texture:'Soft')
+# ch3 = Cheese.create(cheese_description: '', cheese_name: 'Parmigiano Reggiano', milk_type:'Cow Milk', origin_country: 'Italy', pasteurized: false, taste:'Sharp', texture:'Hard')
