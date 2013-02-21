0d4d94263c958a5ded7136f08268ea3b0afd5923
Budi Sugianto <budisugianto777@gmail.com>
2013-02-20 20:47:38 -0500

    Fix the build

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,15 @@ ChooseCheese::Application.routes.draw do
 
   resources :cheeses
 
-  devise_for :users, :skip => [:registrations]
+  devise_for :users, :controllers => {:registrations => "registrations"}, :skip => [:registrations]
   as :user do
     get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'
     put 'users' => 'devise/registrations#update', :as => 'user_registration'
+  end
+
+  devise_scope :user do
+    get "/sign_in",  :to => "devise/sessions#new"
+    get "/sign_up",  :to => "devise/registrations#new"
   end
 
   root :to =>'pages#home'
