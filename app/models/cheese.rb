54b1e951cf01f341b50cfe80f1acec01fa0d8fa4
Budi Sugianto <budisugianto777@gmail.com>
2013-03-08 23:53:58 -0500

    fix the cheese model for s3

--- app/models/cheese.rb
+++ app/models/cheese.rb
@@ -6,10 +6,14 @@ class Cheese < ActiveRecord::Base
   validates_attachment :image, presence: true, content_type: {content_type: ['image/jpg', 'image/jpeg', 'image/png', 'image/gif']}, size: {less_than: 3.megabytes}
 
   has_attached_file :image , :styles => { :medium => "400x300>", :thumb => "100x100>" },
-  :storage => :s3,
+      :storage => :s3,
       :s3_credentials => "#{Rails.root}/config/s3.yml",
-      :path => ":attachment/activities/:id/:style.:extension",
+      :path => "assets/:class/:attachment/:id/:style.:extension",
+      #:path => ":attachment/activities/:id/:style.:extension",
+      :s3_permissions => 'public_read',
+      :s3_protocol => 'http',
       :bucket => 'choose-cheese'
+
 
   #scope :search, lambda{|search| where("cheese_name: LIKE ?", "%#{search}%")}
 
